24c1ae0c2e566afad23a9a6d096677afc4377e80
w-h-a <wesley.herb.anderson@gmail.com>
2024-05-31 19:01:56 -0700

    feat: finish validate

--- internal/task/terraform/options.go
+++ internal/task/terraform/options.go
@@ -6,14 +6,14 @@ import (
 	"github.com/w-h-a/cli/internal/task"
 )
 
-func TerraformWithVars(vars map[string]string) task.TaskOption {
-	return func(o *task.TaskOptions) {
-		o.Context = context.WithValue(o.Context, "tf_vars_key", vars)
-	}
-}
-
 func TerraformWithRemoteStates(rs map[string]string) task.TaskOption {
 	return func(o *task.TaskOptions) {
 		o.Context = context.WithValue(o.Context, "tf_remote_states_key", rs)
+	}
+}
+
+func TerraformWithVars(vars map[string]string) task.TaskOption {
+	return func(o *task.TaskOptions) {
+		o.Context = context.WithValue(o.Context, "tf_vars_key", vars)
 	}
 }
